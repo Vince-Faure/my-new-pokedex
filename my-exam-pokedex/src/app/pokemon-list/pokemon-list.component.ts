d3ae336222704e80c412ed455f0db6119cc04222
vinceFaure <faurvincent@gmail.com>
2023-06-04 10:56:04 +0200

    adding types

--- my-exam-pokedex/src/app/pokemon-list/pokemon-list.component.ts
+++ my-exam-pokedex/src/app/pokemon-list/pokemon-list.component.ts
@@ -11,15 +11,15 @@ import Pokemon from '../types/pokemon.type';
 export class PokemonListComponent implements OnInit {
   
   pokemons: Pokemon[] = [
-    {id: 1, name: 'Salamèche'},
-    {id: 2, name: 'Limagma'},
-    {id: 3, name: 'Malosse'},
-    {id: 4, name: 'Raikou'},
-    {id: 5, name: 'Electable'},
-    {id: 6, name: 'Electhor'},
-    {id: 7, name: 'Torterra'},
-    {id: 8, name: 'Roserade'},
-    {id: 9, name: 'Florizarre'},
+    {id: 1, type:'feu', name: 'Salamèche',taille: '0.6m',evolution: 'Reptincel, Dracaufeu ', faiblesses: 'Eau, Sol, Roche', attaques: 'Nitrocharge, Rebondifeu, Lance-Flammes'},
+    {id: 2, type:'feu', name: 'Limagma',taille: '0.7m',evolution: 'Volcaropode ', faiblesses: 'Eau, Sol, Roche', attaques: 'Nitrocharge, Eboulement'},
+    {id: 3, type:'feu', name: 'Malosse',taille: '0.5m',evolution: 'Demolosse ', faiblesses: 'Eau, Sol, Roche', attaques: 'Feinte, Flammeche, Vibrobscur'},
+    {id: 4, type:'foudre', name: 'Raikou',taille: '0.8m',evolution: 'Super Raiku ', faiblesses: ', Sol, Roche', attaques: 'Change eclair, Rayon Chargé, Elecanon'},
+    {id: 5, type:'foudre', name: 'Electable',taille: '0.6m',evolution: 'Mega Electable ', faiblesses: ' Sol, Roche', attaques: 'Eclair, Tonnerre, coup de jus'},
+    {id: 6, type:'foudre', name: 'Electhor',taille: '0.3m',evolution: 'Suprême Electhor', faiblesses: ' Sol, Roche', attaques: 'Etincelle, Rayon Chargé'},
+    {id: 7,type:'plante', name: 'Torterra',taille: '0.7m',evolution: 'Torterra Senior ', faiblesses: ' Feu, Glace', attaques: 'Tranch-Herbe, Lame-Feuille , Lance-Soleil'},
+    {id: 8,type:'plante', name: 'Roserade',taille: '0.6m',evolution: 'Hyper Roserade ', faiblesses: ' Feu, Glace', attaques: 'Mégafouet, Nœud Herbe, Lame-Feuille'},
+    {id: 9,type:'plante', name: 'Florizarre',taille: '0.9m',evolution: 'Giga Florizarre ', faiblesses: ' Feu, Glace', attaques: 'Canon Graine, Éco-Sphère, Phytomixeur'},
   ];
 
   selectedPokemon : Pokemon;
--- my-exam-pokedex/src/app/types/pokemon.type.ts
+++ my-exam-pokedex/src/app/types/pokemon.type.ts
@@ -2,5 +2,10 @@ export default class Pokemon {
 
     id: number;
     name: string;
+    type: string;
+    taille: string;
+    evolution: string;
+    faiblesses: string;
+    attaques: string;
 
 }
